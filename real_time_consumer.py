c85c919d1d9ab672e39b9057f2d03b9d9078a9b4
zehaoj <zehaoj>
2021-10-23 00:58:20 +0200

    write real-time window data to csv files

--- real_time_consumer.py
+++ real_time_consumer.py
@@ -1,11 +1,11 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql.session import SparkSession
-
+import pandas
 from pyspark.sql.functions import from_json, col
 from pyspark.sql import functions as func
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType, ArrayType
-
+import time
 import findspark
 findspark.init() 
 
@@ -13,16 +13,6 @@ import json
 
 import pymongo
 
-
-
-
-# Create a MongoDB database and collection
-#client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
-#db_name = "arXiv_db"
-#db = client[db_name]
-
-#current_collection = "recent_papers"
-#papers_collection = db[current_collection]
 
 # Create a local StreamingContext with two working thread and batch interval of 3 second
 sc = SparkContext("local", "arXivConsumer")
@@ -44,7 +34,7 @@ df = spark \
 
 # cast value col from binary to String
 df = df.withColumn("value", df["value"].cast("string"))
-df.printSchema()
+# df1.printSchema()
 
 # schema of value column received from Producer
 schema = StructType(
@@ -66,49 +56,70 @@ schema = StructType(
 
 # dataframe that contains papers info
 df_paper_info = df.withColumn("value", from_json("value", schema)) \
-    .select(col('value.*')) 
-# rename column key to _id to use it as primary key of each document in MongoDB
-#df_paper_info = df_paper_info.withColumnRenamed('key', '_id')
+    .select(col('value.*'))
 
-#df_paper_info = df_paper_info.groupBy("main_category").count()
-#df_paper_info = df_paper_info.withColumnRenamed('main_category', '_id')
+# dataframe that contains category paper count within 10s window
+df_num_papers_cat = df_paper_info \
+  .withColumn('time', func.current_timestamp()) \
+  .withWatermark("time", "15 seconds") \
+  .groupby(func.window("time", "10 seconds"), col("main_category")) \
+  .count()
 
-
-'''
-def write_mongo_row(df, epoch_id, db_name=db_name, collection_name=current_collection):
-  mongoURL = "mongodb://127.0.0.1/{}.{}".format(db_name, collection_name)
-  df.write.format("mongo").mode("append").option("uri", mongoURL).save()
-  pass
-
-query=df_paper_info \
-  .writeStream \
-  .outputMode("update") \
-  .foreachBatch(write_mongo_row).start()
-query.awaitTermination()
-'''
-
-df_num_papers_cat = df_paper_info.groupby(df_paper_info.main_category).count()
-
-#val d = inputDataset.groupBy("realkey").agg(mean("realvalue"))
-
+# dataframe that contains category page average count within 10s window
 df_avg_pages_cat = df_paper_info \
-  .filter(df_paper_info.page_num > 0) \
-  .groupBy(df_paper_info.main_category) \
-  .agg(func.mean('page_num'))
+  .withColumn('time', func.current_timestamp()) \
+  .withWatermark("time", "15 seconds") \
+  .filter(col("page_num") > 0) \
+  .groupBy(func.window("time", "10 seconds"), col("main_category")) \
+  .agg(func.mean(col('page_num'))).alias("time")
+
 
 # need to write this, but I get an error
-final_df = df_num_papers_cat.join(df_avg_pages_cat, 
-  df_num_papers_cat.main_category == df_avg_pages_cat.main_category, 'inner')
+#final_df = df_num_papers_cat.join(df_avg_pages_cat, 
+  #df_num_papers_cat.main_category == df_avg_pages_cat.main_category, 'inner')
 
-# write dataframe to terminal to debug
-ds = df_avg_pages_cat \
+
+# write both dataframe to terminal to debug
+'''
+ds = df_num_papers_cat \
   .writeStream \
   .outputMode("complete") \
   .format("console") \
   .start() \
-  .awaitTermination()
+'''
 
-#.outputMode("complete") \
+global_cnt_cnt = 0
+global_avg_cnt = 0
+
+def foreach_batch_cnt(df, epoch_id):
+    global global_cnt_cnt
+    df.persist()
+    pdf = df.toPandas()
+    if not pdf.empty:
+        global_cnt_cnt += 1
+        pdf.to_csv('cnt_csv' + str(global_cnt_cnt) + '.csv')
+    df.unpersist()
+
+def foreach_batch_avg(df, epoch_id):
+    global global_avg_cnt
+    df.persist()
+    pdf = df.toPandas()
+    if not pdf.empty:
+        global_avg_cnt += 1
+        pdf.to_csv('avg_csv' + str(global_avg_cnt) + '.csv')
+    df.unpersist()
+
+def processRow(row):
+    print(row)
+    print("------------")
+
+dss = df_num_papers_cat \
+  .writeStream.foreachBatch(foreach_batch_cnt).start()
+
+ 
+dss2 = df_avg_pages_cat \
+  .writeStream.foreachBatch(foreach_batch_avg).start().awaitTermination()
+
 spark.stop()
 
 
